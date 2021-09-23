e700c41fa5b6a449536bd79dfcb1858d872be6cb
Savion92 <savion90@gmail.com>
2021-09-23 13:01:07 -0400

    adds homepage content/styling, adds navbar

--- client/App.js
+++ client/App.js
@@ -7,6 +7,7 @@ import Routes from './Routes'
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes />
     </div>
   )
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,45 +3,80 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Superior Squirrels</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+// const Navbar = ({handleClick, isLoggedIn}) => (
+//   <div>
+//     <h1>Superior Squirrels</h1>
+//     <nav>
+//       {isLoggedIn ? (
+//         <div>
+//           {/* The navbar will show these links after you log in */}
+//           <Link to="/home"> Home </Link>
+//           <a href="#" onClick={handleClick}>
+//             Logout
+//           </a>
+//         </div>
+//       ) : (
+//         <div>
+//           {/* The navbar will show these links before you log in */}
+//           {/* <Link to="/login">Login</Link> */}
+//           {/* <Link to="/signup">Sign Up</Link> */}
+//         </div>
+//       )}
+//     </nav>
+//     <hr />
+//   </div>
+// )
 
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
-  }
+// const mapState = state => {
+//   return {
+//     isLoggedIn: !!state.auth.id
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     handleClick() {
+//       dispatch(logout())
+//     }
+//   }
+// }
+
+// export default connect(mapState, mapDispatch)(Navbar)
+
+
+const NavBar = () =>{
+  return (
+  <div style={{ textAlign:'center'}}>
+    <h3>SuperiorSquirrelStore</h3>
+    <div >
+      <Link to="/"> Home </Link>
+      <Link to="/products" style={{margin:".5rem"}}>
+        Products
+      </Link>
+
+      <form action="/" style={{display: "inline-block"}} >
+        <input
+        type="text"
+        placeholder="Search for Stuffies"
+        />
+        <button>
+          search
+        </button>
+      </form>
+      <Link style={{margin:".5rem"}} >
+      Login/Sign up
+      </Link>
+      <Link style={{margin:".5rem"}} >
+      Cart
+      </Link>
+    </div>
+
+  </div>
+
+    )
 }
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
+export default NavBar
